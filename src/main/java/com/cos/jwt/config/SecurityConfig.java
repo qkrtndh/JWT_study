ec05bac4704dc36f1ba67582b6f384acc4927a6f
qkrtndh <qkrtndh5@naver.com>
2021-07-23 19:18:13 +0900

    jwt 필터 등록 테스트

--- src/main/java/com/cos/jwt/config/SecurityConfig.java
+++ src/main/java/com/cos/jwt/config/SecurityConfig.java
@@ -5,7 +5,11 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.web.filter.CorsFilter;
+
+import com.cos.jwt.filter.MyFilter1;
+import com.cos.jwt.filter.MyFilter3;
 
 import lombok.RequiredArgsConstructor;
 
@@ -18,6 +22,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
+		http.addFilterBefore(new MyFilter3(),BasicAuthenticationFilter.class);
 		http.csrf().disable();
 		//세션사용하지 않겠다는 의미
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
