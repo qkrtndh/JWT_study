787dee8420b46fb4ed7c7af44b840c27fc9f29e1
qkrtndh <qkrtndh5@naver.com>
2021-07-26 13:35:08 +0900

    jwt 토큰 만들어 응답

--- src/main/java/com/cos/jwt/config/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/cos/jwt/config/jwt/JwtAuthenticationFilter.java
@@ -1,7 +1,10 @@
 package com.cos.jwt.config.jwt;
 
 import java.io.IOException;
+import java.util.Date;
 
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -11,6 +14,8 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
 import com.cos.jwt.config.auth.PrincipalDetails;
 import com.cos.jwt.model.User;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -62,5 +67,22 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
 		
 		
 		return null;
+	}
+	//attemptAuthentication 실행 후 인증이 정상적으로 되었으면  실행되는 함수
+	//JWT 토큰을 만들어서 request 요청한 사용자에게 jwt 토큰을 response 해주면 도미
+	@Override
+	protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain,
+			Authentication authResult) throws IOException, ServletException {
+		System.out.println("success");	
+		PrincipalDetails principalDetails = (PrincipalDetails) authResult.getPrincipal();
+		
+		//RSA방식은 아니고 Hash방식
+		String jwtToken = JWT.create()
+				.withSubject("토큰이름")
+				.withExpiresAt(new Date(System.currentTimeMillis()+(60000*10)))
+				.withClaim("id",principalDetails.getUser().getId())
+				.withClaim("username", principalDetails.getUser().getUsername())
+				.sign(Algorithm.HMAC512("cos"));
+		response.addHeader("Authorization", "Bearer "+jwtToken);
 	}
 }
